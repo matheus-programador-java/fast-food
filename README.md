7b08aedb8f9657785bdbf9af009106f5397bd906
unknown <matheusrfjava@gmail.com>
2020-11-04 09:21:30 -0300

    new file:   .gitignore 	new file:   .idea/.gitignore 	new file:   .idea/.name 	new file:   .idea/codeStyles/Project.xml 	new file:   .idea/codeStyles/codeStyleConfig.xml 	new file:   .idea/compiler.xml 	new file:   .idea/gradle.xml 	new file:   .idea/jarRepositories.xml 	new file:   .idea/misc.xml 	new file:   README.md 	new file:   app/.gitignore 	new file:   app/build.gradle 	new file:   app/proguard-rules.pro 	new file:   app/src/androidTest/java/com/maquina/foodmenu/ExampleInstrumentedTest.kt 	new file:   app/src/main/AndroidManifest.xml 	new file:   app/src/main/java/com/maquina/foodmenu/LancheFrag.kt 	new file:   app/src/main/java/com/maquina/foodmenu/MainActivity.kt 	new file:   app/src/main/res/drawable-v24/ic_launcher_foreground.xml 	new file:   app/src/main/res/drawable/background.xml 	new file:   app/src/main/res/drawable/custom_white_black.xml 	new file:   app/src/main/res/drawable/custom_white_button.xml 	new file:   app/src/main/res/drawable/ic_background_2.xml 	new file:   app/src/main/res/drawable/ic_launcher_background.xml 	new file:   app/src/main/res/drawable/ic_navigation.xml 	new file:   app/src/main/res/drawable/ic_wpp.xml 	new file:   app/src/main/res/font/roboto_black.ttf 	new file:   app/src/main/res/font/roboto_bold.ttf 	new file:   app/src/main/res/font/roboto_light.ttf 	new file:   app/src/main/res/layout/activity_main.xml 	new file:   app/src/main/res/layout/fragment_lanche.xml 	new file:   app/src/main/res/mipmap-anydpi-v26/ic_launcher.xml 	new file:   app/src/main/res/mipmap-anydpi-v26/ic_launcher_round.xml 	new file:   app/src/main/res/mipmap-hdpi/ic_launcher.png 	new file:   app/src/main/res/mipmap-hdpi/ic_launcher_round.png 	new file:   app/src/main/res/mipmap-mdpi/ic_launcher.png 	new file:   app/src/main/res/mipmap-mdpi/ic_launcher_round.png 	new file:   app/src/main/res/mipmap-xhdpi/ic_launcher.png 	new file:   app/src/main/res/mipmap-xhdpi/ic_launcher_round.png 	new file:   app/src/main/res/mipmap-xxhdpi/ic_launcher.png 	new file:   app/src/main/res/mipmap-xxhdpi/ic_launcher_round.png 	new file:   app/src/main/res/mipmap-xxxhdpi/ic_launcher.png 	new file:   app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.png 	new file:   app/src/main/res/values/colors.xml 	new file:   app/src/main/res/values/dimens.xml 	new file:   app/src/main/res/values/strings.xml 	new file:   app/src/main/res/values/themes.xml 	new file:   app/src/test/java/com/maquina/foodmenu/ExampleUnitTest.kt 	new file:   build.gradle 	new file:   gradle.properties 	new file:   gradle/wrapper/gradle-wrapper.jar 	new file:   gradle/wrapper/gradle-wrapper.properties 	new file:   gradlew 	new file:   gradlew.bat 	new file:   settings.gradle

---
+++ README.md
@@ -0,0 +1,1 @@
+<h1>Fast Food Menu</h1>
---
+++ app/src/main/java/com/maquina/foodmenu/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.maquina.foodmenu
+
+import android.content.pm.ActivityInfo
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+    }
+}
