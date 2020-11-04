0ca6a929421a1650a0c2b45b28f6bf2cbea300a1
unknown <matheusrfjava@gmail.com>
2020-11-04 10:08:08 -0300

    new file:   .idea/vcs.xml 	modified:   app/build.gradle 	new file:   app/src/main/java/com/maquina/foodmenu/PizzaFrag.kt 	new file:   app/src/main/java/com/maquina/foodmenu/PorcaoFrag.kt 	modified:   app/src/main/res/layout/activity_main.xml 	modified:   app/src/main/res/layout/fragment_lanche.xml 	new file:   app/src/main/res/layout/fragment_pizza.xml 	new file:   app/src/main/res/layout/fragment_porcao.xml 	new file:   app/src/main/res/navigation/nav_graph.xml

---
+++ app/src/main/java/com/maquina/foodmenu/PorcaoFrag.kt
@@ -0,0 +1,19 @@
+package com.maquina.foodmenu
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class PorcaoFrag : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_porcao, container, false)
+    }
+
+}
